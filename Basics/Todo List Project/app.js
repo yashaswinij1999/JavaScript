2aba8c1912773580f7143c54e692003c1cf0d5e7
Yashaswini J <yashaswinij1999>
2024-03-17 19:32:47 +0530

    initial commit

---
+++ Basics/Todo List Project/app.js
@@ -0,0 +1,11 @@
+let select = prompt("Enter What do you like to do");
+console.log(select);
+
+const todoList = [];
+let task;
+
+while (select != "quit") {}
+
+if (select === "quit") {
+  console.log("YOU QUIT!!!!!");
+}
