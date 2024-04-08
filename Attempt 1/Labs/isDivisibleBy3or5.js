b07abb787b4acb30c8da99d5c1ad65d58f646113
Yashaswini J <yashaswinij1999>
2024-04-08 13:52:35 +0530

    divisibility of 3 and 5

---
+++ Attempt 1/Labs/isDivisibleBy3or5.js
@@ -0,0 +1,7 @@
+function isDivisibleBy3or5(num) {
+  return num % 3 === 0 || num % 5 === 0;
+}
+
+console.log(isDivisibleBy3or5(3));
+console.log(isDivisibleBy3or5(10));
+console.log(isDivisibleBy3or5(8));
