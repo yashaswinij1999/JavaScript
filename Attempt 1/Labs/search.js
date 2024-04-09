b055cf04fece012fa5cf66e0d3025203a598e839
Yashaswini J <yashaswinij1999>
2024-04-09 15:08:33 +0530

    check if the arr contains the required element

---
+++ Attempt 1/Labs/search.js
@@ -0,0 +1,7 @@
+function search(arr, n) {
+  return arr.includes(n);
+}
+
+console.log(search([1, 2, 4, 5, 6, 8], 4));
+console.log(search([2, 4, 5, 3], 8));
+console.log(search([12, 3, 5, 6, 8], 2));
