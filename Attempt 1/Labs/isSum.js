fdac4c557d9fd2f1cbf639784d60fc7873267f02
Yashaswini J <yashaswinij1999>
2024-04-10 12:24:30 +0530

    sum of all digits in a number

--- Attempt 1/Labs/isSum.js
+++ Attempt 1/Labs/isSum.js
@@ -2,13 +2,15 @@ function isSum(num) {
   let r = 0;
   let sum = 0;
 
+  num = Math.abs(num);
+
   while (num > 0) {
     r = num % 10;
     sum = sum + r;
-    num = num / 10;
+    num = Math.floor(num / 10);
   }
   return sum;
 }
 
-console.log(isSum(123));
+console.log(isSum(-123));
 console.log(isSum(333));
