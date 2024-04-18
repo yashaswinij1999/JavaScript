86a4937aa1eb490a4f0f97b187d2ecc6e2689194
Yashaswini J <yashaswinij1999>
2024-04-18 18:26:04 +0530

    check if an consists either 2 or 4 and not both

---
+++ Attempt 1/Labs/either24.js
@@ -0,0 +1,19 @@
+function either24(arr) {
+  if (arr.includes(2) && arr.includes(4)) {
+    return false;
+  } else {
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] === 2 || arr[i] === 4) {
+        if (arr[i] === arr[i + 1]) {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
+}
+
+console.log(either24([4, 4, 1, 2, 2]));
+console.log(either24([1, 2, 2]));
+console.log(either24([4, 4, 1]));
+console.log(either24([1, 5, 9]));
