a3a1b77844ee8269e01e0681e543f237864320a9
Yashaswini J <yashaswinij1999>
2024-04-22 13:24:55 +0530

    done

--- Attempt 2/TV Show API/app.js
+++ Attempt 2/TV Show API/app.js
@@ -1,9 +1,29 @@
 const button = document.querySelector("button");
-const input = document.querySelector("#tv");
 
-button.addEventListener("click", function (e) {
+button.addEventListener("click", async function (e) {
   e.preventDefault();
-  const param = input.value;
-  const response = axios.get("https://api.tvmaze.com/search/shows");
+  const input = document.querySelector("input");
+  const val = input.value;
+  console.log(val);
+  const config = { params: { q: val } };
+  const response = await axios.get(
+    "https://api.tvmaze.com/search/shows",
+    config
+  );
   console.log(response);
+  const shows = response.data;
+  getImage(shows);
+  input.value = "";
 });
+
+const getImage = function (shows) {
+  for (let data of shows) {
+    console.log(data);
+    if (data.show.image) {
+      const img = document.createElement("img");
+      img.src = data.show.image.medium;
+      document.body.append(img);
+      img.style.margin = "10px";
+    }
+  }
+};
