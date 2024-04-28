050b2988f1dd9dc62a74aba054a320e7156bca52
Yashaswini J <yashaswinij1999>
2024-04-28 14:44:31 +0530

    check if array contains 1 or 3 if yes return false

---
+++ Attempt 1/Labs/lucky13.js
@@ -0,0 +1,9 @@
+function lucky13(arr) {
+  if (arr.includes(1) || arr.includes[3]) {
+    return false;
+  }
+  return true;
+}
+console.log(lucky13([0, 2, 4]));
+console.log(lucky13([1, 2, 3]));
+console.log(lucky13([1, 2, 4]));
