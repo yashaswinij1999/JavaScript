4dae70b43a2cbddccd00c70e43b7bae15c0ca0e1
Yashaswini J <yashaswinij1999>
2024-07-24 12:19:38 +0530

    checking whether the matrix hass same number of rows and colums

--- Attempt 1/Labs/listSort.js
+++ Attempt 1/Labs/listSort.js
@@ -1,4 +1,26 @@
 function listSort(str) {
   return str.sort();
 }
-console.log(listSort(["Word", "Java", "Computer", "Object", "Software"]));
+// console.log(listSort(["Word", "Java", "Computer", "Object", "Software"]));
+
+function remove(str) {
+  let count = 0;
+  let duplicate = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    count = 0;
+
+    for (let j = 0; j < str.length; j++) {
+      if (str[i] === str[j]) {
+        count++;
+      }
+    }
+
+    if (count > 1) {
+      duplicate = str[i];
+    }
+  }
+  return duplicate;
+}
+
+console.log(remove([1, 2, 3, 2, 1, 2]));
